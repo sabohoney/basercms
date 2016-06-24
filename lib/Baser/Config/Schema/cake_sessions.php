1382d07c35afcb2f28d681f8bd2519604d5aa254
Naoki Iha <n_iha@wan55.co.jp>
2016-06-24 14:28:44 +0900

    CakeSession is the schema for the session management.

---
+++ lib/Baser/Config/Schema/cake_sessions.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Dblogs schema generated on: 2011-06-12 11:06:13 : 1307847253 */
+
+class CakeSessionsSchema extends CakeSchema {
+
+	public $name = 'CakeSessions';
+
+	public $file = 'cake_sessions.php';
+
+	public $connection = 'baser';
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $dblogs = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'key' => 'primary'),
+		'date' => array('type' => 'text', 'null' => true, 'default' => null),
+		'expires' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 11),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci')
+	);
+
+}
